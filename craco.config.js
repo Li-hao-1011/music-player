73fa82933296e9d14d101e83def64b50951510f7
lihao <lihao.coder.foxmail.com>
2022-12-11 00:40:50 +0800

    css样式重置

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,10 @@
+const cracoLess = require('craco-less')
 const path = require('path')
 
 const resolve = (dir) => path.resolve(__dirname, dir)
 
 module.exports = {
+  plugins: [{ plugin: cracoLess }],
   webpack: {
     alias: {
       '@': resolve('src')
